ee9da9df36edf001d726db06d0c5599326d0f753
Mikhail <tiunovmixs@gmail.com>
2025-06-18 13:59:44 +0300

    Add endpoints for creation, updating and view profile info

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
