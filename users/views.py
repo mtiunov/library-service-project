bfebb33ba48291d8f0f2c16b451ed1e1f09f491a
Mikhail <tiunovmixs@gmail.com>
2025-06-26 00:15:39 +0300

    Added permissions for model Book and Users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from users.serializers import UserSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
@@ -8,6 +10,7 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
