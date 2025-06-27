ad49eb0181e7decbc16472aea9ca88ee6c8c6827
Mikhail <tiunovmixs@gmail.com>
2025-06-27 14:36:57 +0300

    Implement Borrowing model

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from borrowings.models import Borrowing
 
-# Register your models here.
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "book",
+        "user",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+    )
+    list_filter = ("borrow_date", "expected_return_date", "actual_return_date")
+    search_fields = ("book__title", "user__email")
+    raw_id_fields = ("book", "user")
--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from books.models import Book
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name="borrowings")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="borrowings"
+    )
+
+    class Meta:
+        ordering = ["borrow_date"]
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name} borrows " \
+               f"{self.book.title} till {self.expected_return_date}"
+
+    def clean(self):
+        if self.expected_return_date < self.borrow_date:
+            raise ValidationError(
+                {"expected_return_date": "Expected return date cannot "
+                                         "be earlier than borrow date."}
+            )
+        if self.actual_return_date and self.actual_return_date < self.borrow_date:
+            raise ValidationError(
+                {"actual_return_date": "Actual return date cannot "
+                                       "be earlier than borrow date."}
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
