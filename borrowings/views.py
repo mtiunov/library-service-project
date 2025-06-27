6cfd9f9d019de44d828c57a26cec903ad4699d45
Mikhail <tiunovmixs@gmail.com>
2025-06-27 23:52:23 +0300

    Implement borrowing list api

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingListSerializer
 
-# Create your views here.
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingListSerializer
